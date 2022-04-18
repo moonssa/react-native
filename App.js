99b4bb2127b2f7ce7d083dd921c740c1ae5aaa52
moonk <mkjoehsb@gmail.com>
2022-04-19 06:51:04 +0900

    layout

--- App.js
+++ App.js
@@ -4,22 +4,10 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}> Hello!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, flexDirection: "row" }}>
+      <View style={{ flex: 1, backgroundColor: "orange" }}></View>
+      <View style={{ flex: 1, backgroundColor: "teal" }}></View>
+      <View style={{ flex: 1, backgroundColor: "tomato" }}></View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    fontSize: 48,
-    color: "red",
-  },
-});
